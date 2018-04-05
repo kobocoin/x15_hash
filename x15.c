b2e7da500ebac807d875d3184b8a0ef0b66d41a1
Felix Ugoji <10263840+TheTribesman@users.noreply.github.com>
2018-04-05 01:15:05 +0100

    Update x15.c

--- x15.c
+++ x15.c
@@ -25,9 +25,9 @@ void x15_hash(const char* input, char* output)
     sph_blake512_context     ctx_blake;
     sph_bmw512_context       ctx_bmw;
     sph_groestl512_context   ctx_groestl;
+    sph_skein512_context     ctx_skein;
     sph_jh512_context        ctx_jh;
     sph_keccak512_context    ctx_keccak;
-    sph_skein512_context     ctx_skein;
     sph_luffa512_context     ctx_luffa;
     sph_cubehash512_context  ctx_cubehash;
     sph_shavite512_context   ctx_shavite;
@@ -53,6 +53,10 @@ void x15_hash(const char* input, char* output)
     sph_groestl512 (&ctx_groestl, hashB, 64);
     sph_groestl512_close(&ctx_groestl, hashA);
 
+    sph_skein512_init(&ctx_skein);
+    sph_skein512 (&ctx_skein, hashA, 64);
+    sph_skein512_close (&ctx_skein, hashB);
+
     sph_jh512_init(&ctx_jh);
     sph_jh512 (&ctx_jh, hashB, 64);
     sph_jh512_close(&ctx_jh, hashA);
@@ -60,10 +64,6 @@ void x15_hash(const char* input, char* output)
     sph_keccak512_init(&ctx_keccak);
     sph_keccak512 (&ctx_keccak, hashA, 64);
     sph_keccak512_close(&ctx_keccak, hashB);
-
-    sph_skein512_init(&ctx_skein);
-    sph_skein512 (&ctx_skein, hashA, 64);
-    sph_skein512_close (&ctx_skein, hashB);
 	
     sph_luffa512_init (&ctx_luffa);
     sph_luffa512 (&ctx_luffa, hashB, 64);
